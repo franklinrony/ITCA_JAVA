8fe3496ff5c133a6b5bd666978b09aad0470c5cb
franklinrony <franklinrony2@gmail.com>
2015-06-13 16:10:14 -0600

    Commit inicial

---
+++ src/prueba_java.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)prueba_java.java
+ *
+ * prueba_java application
+ *
+ * @author 
+ * @version 1.00 2015/6/13
+ */
+ 
+public class prueba_java {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
